1add07ccb9094a048f583f13883bbc1e0094c31a
Leonardo Galli <leonardo.galli@bluewin.ch>
2017-06-01 00:46:12 +0200

    Pls cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -56,6 +56,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'cors' => 'App\Http\Middleware\CORS',
+        'cors' => \App\Http\Middleware\CORS::class,
     ];
 }
