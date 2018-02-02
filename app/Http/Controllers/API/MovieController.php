3583e1b34177d21655cf96132911bb0049e6541b
Leonardo Galli <leonardo.galli@bluewin.ch>
2018-02-02 15:09:29 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/API/MovieController.php
+++ app/Http/Controllers/API/MovieController.php
@@ -2,18 +2,18 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Http\Controllers\Controller;
 use App\Movie;
 use Carbon\Carbon;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input;
-use App\Http\Controllers\Controller;
 
 class MovieController extends Controller
 {
     public function refresh_movies()
     {
-        $ids = Input::get("ids");
-        $movies = Movie::whereIn("id", $ids)->where("updated_at", ">", Carbon::now()->subDay());
+        $ids = Input::get('ids');
+        $movies = Movie::whereIn('id', $ids)->where('updated_at', '>', Carbon::now()->subDay());
+
         return $movies->paginate(250);
     }
 
