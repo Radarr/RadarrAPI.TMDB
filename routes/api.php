d9daaa68796b74a07143a624c38e624a5a1e2fdc
Leonardo Galli <leonardo.galli@bluewin.ch>
2018-03-07 18:21:53 +0000

    Apply fixes from StyleCI

--- routes/api.php
+++ routes/api.php
@@ -18,42 +18,40 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get("/ping", "API\JSONController@ping");
+Route::get('/ping', "API\JSONController@ping");
 
-Route::get("/hello", "API\TestJSONController@hello");
+Route::get('/hello', "API\TestJSONController@hello");
 
+Route::get('/discovery/upcoming', "API\DiscoverController@upcoming");
 
-Route::get("/discovery/upcoming", "API\DiscoverController@upcoming");
+Route::get('/discovery/popular', "API\DiscoverController@popular");
 
-Route::get("/discovery/popular", "API\DiscoverController@popular");
+Route::get('/discovery/recommendations', "API\DiscoverController@recommendations");
 
-Route::get("/discovery/recommendations", "API\DiscoverController@recommendations");
+Route::post('/discovery/upcoming', "API\DiscoverController@upcoming");
 
-Route::post("/discovery/upcoming", "API\DiscoverController@upcoming");
+Route::post('/discovery/popular', "API\DiscoverController@popular");
 
-Route::post("/discovery/popular", "API\DiscoverController@popular");
+Route::post('/discovery/recommendations', "API\DiscoverController@recommendations");
 
-Route::post("/discovery/recommendations", "API\DiscoverController@recommendations");
+Route::get('/search/suggestions', "API\SearchController@suggestions");
 
-Route::get("/search/suggestions", "API\SearchController@suggestions");
+if (Config::get('app.mappings.enabled') === true) {
+    Route::get('/mappings/get', "API\MappingsController@get");
 
-if (Config::get("app.mappings.enabled") === true)
-{
-    Route::get("/mappings/get", "API\MappingsController@get");
+    Route::get('/mappings/add', "API\MappingsController@add");
 
-    Route::get("/mappings/add", "API\MappingsController@add");
+    Route::get('/mappings/vote', "API\MappingsController@vote");
 
-    Route::get("/mappings/vote", "API\MappingsController@vote");
+    Route::get('/mappings/latest', "API\MappingsController@latest");
 
-    Route::get("/mappings/latest", "API\MappingsController@latest");
-
-    Route::get("/mappings/find", "API\MappingsController@find");
+    Route::get('/mappings/find', "API\MappingsController@find");
 }
 
-Route::get("/imdb/top250", "API\IMDBController@top250");
+Route::get('/imdb/top250', "API\IMDBController@top250");
 
-Route::get("/imdb/popular", "API\IMDBController@popular");
+Route::get('/imdb/popular', "API\IMDBController@popular");
 
-Route::get("/imdb/list", "API\IMDBController@user_list");
+Route::get('/imdb/list', "API\IMDBController@user_list");
 
 //Route::get("/maintenance/activate", "DBMaintenanceController@activate");
