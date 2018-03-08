853147fb5350f3d94d27ecdb974047fdc9df71db
Leonardo Galli <leonardo.galli@bluewin.ch>
2018-03-08 21:06:50 +0000

    Apply fixes from StyleCI

--- app/Http/Middleware/CheckDBMaint.php
+++ app/Http/Middleware/CheckDBMaint.php
@@ -10,28 +10,28 @@ class CheckDBMaint
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure                 $next
+     *
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-      $request->attributes->add(["is_db_maintenance" => $this->is_db_maintenance()]);
-      $response = $next($request);
+        $request->attributes->add(['is_db_maintenance' => $this->is_db_maintenance()]);
+        $response = $next($request);
 
-      if ($this->is_db_maintenance())
-      {
-          $response = $this->add_warning($response);
-      }
+        if ($this->is_db_maintenance()) {
+            $response = $this->add_warning($response);
+        }
 
-      //dd("d");
+        //dd("d");
 
-      return $response;
+        return $response;
     }
 
     public function is_db_maintenance()
     {
-        return Storage::disk()->exists("db.down");
+        return Storage::disk()->exists('db.down');
     }
 
     public function add_warning($response)
