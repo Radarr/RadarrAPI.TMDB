3c85f5d2679442b41b76be9495d68f85bc6db326
Leonardo Galli <leonardo.galli@bluewin.ch>
2018-03-19 15:47:30 +0000

    Apply fixes from StyleCI

--- app/Event.php
+++ app/Event.php
@@ -8,36 +8,41 @@ class Event extends Model
 {
     protected $connection = 'mappings_mysql';
 
-    protected $fillable = ["type", "mappings_id", "ip"];
+    protected $fillable = ['type', 'mappings_id', 'ip'];
 
     public $timestamps = false;
 
     public function mapping()
     {
-        return $this->hasOne("App\Mapping", "id", "mappings_id");
+        return $this->hasOne("App\Mapping", 'id', 'mappings_id');
     }
 
     public function toArray()
     {
         $arr = parent::toArray();
 
-        $arr["mapping"] = $this->mapping->toArray();
-        unset($arr["ip"]);
-        $arr["mapping"]["movie"] = MappingMovie::find($arr["mapping"]["tmdbid"]);
+        $arr['mapping'] = $this->mapping->toArray();
+        unset($arr['ip']);
+        $arr['mapping']['movie'] = MappingMovie::find($arr['mapping']['tmdbid']);
+
         return $arr;
     }
 }
 
-abstract class EventType extends Enum {
+abstract class EventType extends Enum
+{
     const AddedMapping = 0;
     const ApproveMapping = 1;
     const DisapproveMapping = 2;
     const LockedMapping = 3;
 }
 
-abstract class Enum {
-    static function getKeys(){
+abstract class Enum
+{
+    public static function getKeys()
+    {
         $class = new ReflectionClass(get_called_class());
+
         return array_keys($class->getConstants());
     }
 }
--- app/Http/Middleware/CheckDBMaintAPI.php
+++ app/Http/Middleware/CheckDBMaintAPI.php
@@ -2,15 +2,13 @@
 
 namespace App\Http\Middleware;
 
-use Closure;
-use Illuminate\Support\Facades\Storage;
-
 class CheckDBMaintAPI extends CheckDBMaint
 {
     public function add_warning($response)
     {
         $orig = $response->original;
-        $orig["warnings"] = array("title" => "Database Maintenance", "details" => "Currently performing Database Maintenance. Data may be stale. No Data may be written to API.");
+        $orig['warnings'] = ['title' => 'Database Maintenance', 'details' => 'Currently performing Database Maintenance. Data may be stale. No Data may be written to API.'];
+
         return $response->setData($orig);
     }
 }
