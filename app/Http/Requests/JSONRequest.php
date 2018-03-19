9f367796cabcbdf93069a85cdb7d0db581856dd5
Leonardo Galli <leonardo.galli@bluewin.ch>
2018-03-19 14:14:11 +0000

    Apply fixes from StyleCI

--- app/Http/Requests/JSONRequest.php
+++ app/Http/Requests/JSONRequest.php
@@ -2,12 +2,10 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Http\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
-use Illuminate\Support\Facades\Log;
-
 use App\Helpers\Helper;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\Request;
 
 class JSONRequest extends FormRequest
 {
@@ -24,28 +22,29 @@ class JSONRequest extends FormRequest
     /**
      * Get the proper failed validation response for the request.
      *
-     * @param  array  $errors
+     * @param array $errors
+     *
      * @return \Symfony\Component\HttpFoundation\Response
      */
     public function response(array $errors)
     {
         if ($this->expectsJson()) {
-          $json_errors = array();
-          foreach ($errors as $key => $error)
-          {
-              $new_error = array();
-              $new_error["id"] = Helper::generate_uuid_v4();
-              $new_error["status"] = 422;
-              $new_error["title"] = "Unprocessable Entity! Parameter {$key} has one or more errors.";
-              $new_error["detail"] = "The following errors happened while processing the request parameter $key: ".implode(" ", $error);
-              $json_errors[] = $new_error;
-          }
-          Log::warning("Unprocessable Entity (422)!", array("errors" => $json_errors, "identification" => array("id" => $json_errors[0]["id"])));
-          return response()->json(array("errors" => $json_errors), 422);
+            $json_errors = [];
+            foreach ($errors as $key => $error) {
+                $new_error = [];
+                $new_error['id'] = Helper::generate_uuid_v4();
+                $new_error['status'] = 422;
+                $new_error['title'] = "Unprocessable Entity! Parameter {$key} has one or more errors.";
+                $new_error['detail'] = "The following errors happened while processing the request parameter $key: ".implode(' ', $error);
+                $json_errors[] = $new_error;
+            }
+            Log::warning('Unprocessable Entity (422)!', ['errors' => $json_errors, 'identification' => ['id' => $json_errors[0]['id']]]);
+
+            return response()->json(['errors' => $json_errors], 422);
         }
+
         return $this->redirector->to($this->getRedirectUrl())
                                         ->withInput($this->except($this->dontFlash))
                                         ->withErrors($errors, $this->errorBag);
     }
-
 }
