5295709812550cae55f75c9c462950897fcf4c4b
Leonardo Galli <leonardo.galli@bluewin.ch>
2020-07-30 21:37:58 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/API/TestJSONController.php
+++ app/Http/Controllers/API/TestJSONController.php
@@ -2,33 +2,25 @@
 
 namespace App\Http\Controllers\API;
 
-use App\User;
-use App\Http\Controllers\Controller;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use App\Helpers\IMDBAPI;
-use Illuminate\Support\Facades\DB;
-use App\Movie;
 
 class TestJSONController extends JSONController
 {
     /**
      * Returns a json string for api usage.
      *
-     * @param  int  $id
+     * @param int $id
+     *
      * @return Response
      */
-	 public function hello($name = "Leo") {
-		 $resp = array();
-		 $resp["message"] = "Hello $name";
+    public function hello($name = 'Leo')
+    {
+        $resp = [];
+        $resp['message'] = "Hello $name";
 
-
-		 return response()->json($resp);
-	 }
+        return response()->json($resp);
+    }
 }
-
-
-
 
 ?>
 
