83e3f3498ac9e09ca7728f68ad9304e45d67b659
Leonardo Galli <galli-leo@users.noreply.github.com>
2020-09-04 14:35:05 +0000

    Apply fixes from StyleCI

--- app/Mapping.php
+++ app/Mapping.php
@@ -2,24 +2,19 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\Relation;
-use Illuminate\Support\Facades\Cache;
-use Helper;
-use MappingsCache;
 use Carbon\Carbon;
-
+use Helper;
+use Illuminate\Database\Eloquent\Model;
 
 class Mapping extends Model
 {
     protected $connection = 'mappings_mysql';
 
-    protected $fillable = array('tmdbid', "info_type", "info_id");
+    protected $fillable = ['tmdbid', 'info_type', 'info_id'];
 
     protected $casts = [
-        "locked" => "boolean",
+        'locked' => 'boolean',
     ];
-
 
     //
 
@@ -32,35 +27,35 @@ class Mapping extends Model
 
     public function info()
     {
-        if ($this->info_type == "title")
-        {
-            return $this->hasOne("App\TitleInfo", "id", "info_id");
+        if ($this->info_type == 'title') {
+            return $this->hasOne("App\TitleInfo", 'id', 'info_id');
         }
 
-        return $this->hasOne("App\YearInfo", "id", "info_id");
+        return $this->hasOne("App\YearInfo", 'id', 'info_id');
     }
 
     public function title_info()
     {
-        return $this->hasOne("App\TitleInfo", "id", "info_id");
+        return $this->hasOne("App\TitleInfo", 'id', 'info_id');
     }
 
     public function year_info()
     {
-        return $this->hasOne("App\YearInfo", "id", "info_id");
+        return $this->hasOne("App\YearInfo", 'id', 'info_id');
     }
 
     public function toArray()
     {
         $arr = parent::toArray();
-        $arr["info"] = $this->info->toArray();
+        $arr['info'] = $this->info->toArray();
         $total = $this->vote_count;
 
         //$random_variation = round($total / 10.0);
 
-        $variation = 0;//random_int(-$random_variation, $random_variation);
-        $arr["votes"] += $variation;
-        $arr["vote_count"] += abs($variation);
+        $variation = 0; //random_int(-$random_variation, $random_variation);
+        $arr['votes'] += $variation;
+        $arr['vote_count'] += abs($variation);
+
         return $arr;
     }
 
@@ -78,36 +73,33 @@ class Mapping extends Model
 
     public function vote($direction = 1)
     {
-        if ($this->locked)
-        {
+        if ($this->locked) {
             return;
         }
 
         $ip = md5($_SERVER['REMOTE_ADDR']);
-        if (Event::whereIn("type", [EventType::AddedMapping, EventType::ApproveMapping, EventType::DisapproveMapping])->where("mappings_id", "=", $this->id)->where("ip", "=", $ip)->whereBetween("date", array(Carbon::now()->addDays(-1), Carbon::now()))->first())
-        {
+        if (Event::whereIn('type', [EventType::AddedMapping, EventType::ApproveMapping, EventType::DisapproveMapping])->where('mappings_id', '=', $this->id)->where('ip', '=', $ip)->whereBetween('date', [Carbon::now()->addDays(-1), Carbon::now()])->first()) {
             return;
         }
         $this->votes = $this->votes + $direction;
         $this->vote_count += 1;
         $event_type = EventType::ApproveMapping;
-        if ($direction == -1)
-        {
+        if ($direction == -1) {
             $event_type = EventType::DisapproveMapping;
         }
-        $event = new Event(["type" => $event_type, "mappings_id" => $this->id, "ip" => $ip]);
+        $event = new Event(['type' => $event_type, 'mappings_id' => $this->id, 'ip' => $ip]);
         $event->save();
         $this->save();
     }
-
 }
 
-class TitleInfo extends Model {
-    protected $table = "title_mappings";
+class TitleInfo extends Model
+{
+    protected $table = 'title_mappings';
 
     protected $connection = 'mappings_mysql';
 
-    protected $fillable = array('aka_title', "aka_clean_title", "language");
+    protected $fillable = ['aka_title', 'aka_clean_title', 'language'];
 
     /**
      * Indicates if the model should be timestamped.
@@ -116,26 +108,24 @@ class TitleInfo extends Model {
      */
     public $timestamps = false;
 
-    public function __construct(array $attributes = array(), $value =null)
+    public function __construct(array $attributes = [], $value = null)
     {
-        if (array_key_exists("aka_title", $attributes) && !array_key_exists("aka_clean_title", $attributes))
-        {
-            $aka_title = $attributes["aka_title"];
-            $attributes["aka_clean_title"] = Helper::clean_title($aka_title);
+        if (array_key_exists('aka_title', $attributes) && !array_key_exists('aka_clean_title', $attributes)) {
+            $aka_title = $attributes['aka_title'];
+            $attributes['aka_clean_title'] = Helper::clean_title($aka_title);
         }
         /* override your model constructor */
         parent::__construct($attributes);
-
     }
-
 }
 
-class YearInfo extends Model {
-    protected $table = "year_mappings";
+class YearInfo extends Model
+{
+    protected $table = 'year_mappings';
 
     protected $connection = 'mappings_mysql';
 
-    protected $fillable = array('aka_year');
+    protected $fillable = ['aka_year'];
 
     /**
      * Indicates if the model should be timestamped.
@@ -143,5 +133,4 @@ class YearInfo extends Model {
      * @var bool
      */
     public $timestamps = false;
-
 }
